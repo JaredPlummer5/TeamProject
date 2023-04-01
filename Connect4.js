9322e373064a736a672b5d6290a81fee201c8515
Codingmasterfred <fredrickg777@gmail.com>
2023-03-31 17:05:05 -0700

    Jared Up nexts

--- Connect4.js
+++ Connect4.js
@@ -1,21 +1,40 @@
 let Connect4Parent = document.getElementById("Connect4");
 
+let slot = function (postition, filledState, neighbors) {
+    this.postition = postition;// where it is on the board
+    this.filledState = null;// changes to the color of the piece
+    this.neighbors = neighbors;// array surrounding pieces
+
+}
+
+let game = function(){
+    this.slots = slots;// this should be an array
+}
+
+
+let objectName = new slot({}, null, [{x: 1, y: 2},{x: 2, y: 1},{x: 2, y: 2}]);
+
 function board() {
-    for (let j = 1; j < 43; j++) {
+
+    for (let j = 1; j <= 7; j++) {
         let placeHolderParent = document.createElement("div")
 
-        for (let i = 0; i < 42; i++) {
+        for (let i = 6; i >= 1; i--) {
+
             let placeHolder = document.createElement("div")
-            placeHolder.id = `PlaceHolder${j}`;
+            placeHolder.id = `PlaceHolder${j}x${i}`;
             placeHolder.className = `PlaceHolders`;
-            placeHolder.innerHTML = `PlaceHolder${j}`;
+            placeHolder.innerHTML = `PlaceHolder${j}x${i}`;
+            placeHolderParent.className = `PlaceHoldersParent`;
             placeHolderParent.append(placeHolder);
-            break;
+            objectName.postition = {x: j, y: i};
+            // We need to create an array of objects and the index of i instead of putting object name
+
         }
-        placeHolderParent.id = `PlaceHolder${[j]}Parent`;
-        placeHolderParent.className = `PlaceHoldersParent`;
+        placeHolderParent.id = `PlaceHolder${j}Parent`;
         Connect4Parent.append(placeHolderParent);
     }
+
 }
 board()
 
