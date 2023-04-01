017c5547a7d2bb8cedc439a222b0fa2ae34bc08a
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-04-01 15:56:38 -0500

    Add eventlisteners to the columns so that when the column is clicked it fills the next empty column from bottom to top

--- Connect4.js
+++ Connect4.js
@@ -35,21 +35,13 @@ function board() {
         placeHolderParent.clicks = 1;
 
         placeHolderParent.addEventListener("click", function (event) {
-            let rowNumber = event.target.id.slice(-1);
             let pieceHeight = placeHolderParent.clicks;
-            let slotClassArray = Array.from(event.target.classList)
-            if (slotClassArray.includes('PlaceHolders')) {
-                
-                console.log(event.target.id.slice(-1), pieceHeight);
-                if (rowNumber == pieceHeight) {
-                    
-                    console.log(event.target);
-                    SelectedPlayer(event);
-                    placeHolderParent.clicks++;
-                }
-
-            }
-
+            let slotToFill = document.querySelector(`#PlaceHolder${placeHolderParent.id.slice(-7, -6)}x${placeHolderParent.clicks}`)
+            console.log('look', slotToFill)
+            console.log(event.target.id.slice(-1), pieceHeight);
+            console.log(event.target);
+            SelectedPlayer(slotToFill);
+            placeHolderParent.clicks++;
         });
 
         for (let i = 6; i >= 1; i--) {
@@ -70,10 +62,10 @@ function board() {
             let newSlot = new slot({}, null, [{ x: 1, y: 2 }, { x: 2, y: 1 }, { x: 2, y: 2 }]);
             newSlot.placeHolder = placeHolder.id
 
-            //console.log(newSlot);
+
             newSlot.postition = { x: j, y: i };
             slotsArray.push(newSlot);
-            //console.log(newSlot.postition);
+
             // We need to create an array of objects and the index of i instead of putting object name
 
         }
@@ -83,50 +75,24 @@ function board() {
     }
 
 }
-
 board();
 
 console.log(slotsArray);
 
 let Turn = 1;
-function SelectedPlayer(event) {
+function SelectedPlayer(slotToFill) {
     let pieces = null;
-    let ClasslistArray = Array.from(event.target.classList)
-    if (ClasslistArray.includes("PlaceHolders")) {
-        if (event.target.style.backgroundColor == "") {
-            console.log(event.target.style)
-            if ((Turn % 2) === 1) {
-                pieces = "red";
+    if ((Turn % 2) === 1) {
+        pieces = "red";
 
-            } else if ((Turn % 2) === 0) {
-                pieces = "blue";
-            } else {
-                pieces = null;
-            }
-
-
-            event.target.style.backgroundColor = pieces;
-            //let positionArray = slotsArray.map(postition => postition.postition)
-            //console.log("This is a array of positions",positionArray);
-
-
-            for (let i = 0; i < slotsArray.length; i++) {
-
-                if (slotsArray[i].color !== pieces && event.target.id == slotsArray[i].placeHolder) {
-
-                    slotsArray[i].color = pieces;
-
-                }
-                else {
-                    console.log("youre here")
-                }
-
-
-            }
-            console.log(slotsArray);
-            Turn++
-        }
+    } else if ((Turn % 2) === 0) {
+        pieces = "blue";
+    } else {
+        pieces = null;
     }
+    slotToFill.style.backgroundColor = pieces;
+    Turn++
 }
+
 //Connect4Parent.addEventListener("click", SelectedPlayer)
 
